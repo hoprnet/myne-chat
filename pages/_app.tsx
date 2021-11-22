dbc4be82c047bf1dddc935d5b9d6b651b30419e0
nionis <stivennoni77@gmail.com>
2021-11-22 13:33:03 +0200

    various improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Grommet, Main } from "grommet";
 import { createGlobalStyle } from "styled-components";
 import theme from "../src/theme";
-import Head from "../src/components/head";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -10,13 +10,25 @@ const GlobalStyle = createGlobalStyle`
     src: url("fonts/GerstnerProgrammFSL/GerstnerProgrammFSL-Regular.otf") format("opentype");
     font-weight: normal;
     font-style: normal;
+    font-display: swap;
   }
 `;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Grommet full theme={theme}>
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        {/* TODO: add more icons */}
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Changing messaging privacy for good."
+        />
+        <title>MYNE Chat</title>
+      </Head>
       <GlobalStyle />
       <Main background="dark-2">
         <Component {...pageProps} />
