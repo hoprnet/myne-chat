9c0861222eb515626e4fcb16ab16b11063b0a7c5
Jose Aguinaga <me@jjperezaguinaga.com>
2021-12-21 05:38:43 -0600

    feat(conversations-panel): added version and git hash for UI
    UI now has a way for users to know and verify which version of Mynechat they are running
    
    Fixes #54

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MYNE_CHAT_GIT_HASH: process.env.MYNE_CHAT_GIT_HASH,
+    NEXT_PUBLIC_MYNE_CHAT_ENVIRONMENT: process.env.NODE_ENV
+  }
 };
