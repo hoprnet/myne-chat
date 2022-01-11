fbcc26f3cd5ab6e13338d85116d49196d4bc4aa1
Jose Aguinaga <me@jjperezaguinaga.com>
2022-01-11 13:34:18 -0600

    test(state): initial setup for state & component testing
    Basic work for component driven testing via jest and react testing library (RTL)
    
    See #36

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import HomePage from '../pages/index'
+import * as nextRouter from 'next/router';
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    const globalRef: any = global;
+    globalRef.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({}),
+      })
+    );
+    // @TODO: Actually enable render for testing
+    // render(<HomePage />)
+  })
+})
