5c1336a5b8520a2f87a021b0b5cc41c797e08d4a
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-02-02 22:14:49 -0600

    Change label alignment

--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -1,12 +1,19 @@
 import type { FunctionComponent, KeyboardEvent } from "react";
 import { useState, useContext } from "react";
-import { Box, Button, TextArea, ResponsiveContext, CheckBox, Tip } from "grommet";
+import {
+  Box,
+  Button,
+  TextArea,
+  ResponsiveContext,
+  CheckBox,
+  Tip,
+} from "grommet";
 
 const ChatInput: FunctionComponent<{
   sendMessage: (destination: string, message: string) => void;
   selection?: string;
   setVerified: (verified: boolean) => void;
-  verified: boolean
+  verified: boolean;
 }> = ({ sendMessage, selection, setVerified, verified }) => {
   const screenSize = useContext(ResponsiveContext);
   const [content, setMessage] = useState<string>("");
@@ -68,13 +75,22 @@ const ChatInput: FunctionComponent<{
           disabled={disableSend}
           onClick={handleSendMessage}
         />
-        <Tip content="Your messages will be signed with your node's key.">
-          <CheckBox
-            pad={{ top: "10px"}}
-            checked={verified}
-            label="Verify?"
-            onChange={(event) => setVerified(event.target.checked)}
-          />
+        <Tip
+          content="Your messages will be signed with your node's key."
+          dropProps={{
+            align: {
+              right: "left",
+            },
+          }}
+        >
+          <Box margin="xxsmall">
+            <CheckBox
+              pad={{ top: "10px" }}
+              checked={verified}
+              label="Verify?"
+              onChange={(event) => setVerified(event.target.checked)}
+            />
+          </Box>
         </Tip>
       </Box>
     </Box>
