62ea9e5ea5b292dfd5f93e530d09558d29a65187
Arsen A. Hutsal <aahutsal@gmail.com>
2022-06-24 14:39:58 +0300

    rewrites applied

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const rewrites = () => {
+    return [
+        {
+            source: "/api/v2/:path*",
+            destination: `${process.env.HOPR_NODE_HTTP_URL || 'http://localhost:13301'}/api/v2/:path*`,
+        },
+    ];
+};
+
 module.exports = {
   reactStrictMode: true,
+  rewrites,
   env: {
     NEXT_PUBLIC_MYNE_CHAT_GIT_HASH: process.env.MYNE_CHAT_GIT_HASH,
     NEXT_PUBLIC_MYNE_CHAT_ENVIRONMENT: process.env.NODE_ENV
