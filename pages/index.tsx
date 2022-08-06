b28fd9bb0b198b1debe0431876387fb8a8e34636
Mariusz Fudali <szczebel1995@gmail.com>
2022-08-06 08:34:50 +0200

    Extend Myne.Chat To Include Earning Indicators Bounty (#142)
    * save before animating canvas coin
    
    * save before untying animation speed from framerate
    
    * first version of coin rain animation
    
    * final cleaning
    
    * cleaned up
    
    Co-authored-by: Steve <stivennoni77@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import ConversationsPanel from "../src/components/conversations-panel";
 import Chat from "../src/components/chat";
 import useWebsocket from "../src/state/websocket";
 import useUser from "../src/state/user";
-
+import { useBalanceListener } from "../src/state/coins";
 
 const HomePage: NextPage = () => {
   const {
@@ -23,8 +23,9 @@ const HomePage: NextPage = () => {
     handleSendMessage,
     handleReceivedMessage,
     loadDevHelperConversation,
-    loadWelcomeConversation
+    loadWelcomeConversation,
   } = useAppState();
+  const { hoprBalance } = useBalanceListener(settings);
   // initialize websocket connection & state tracking
   const websocket = useWebsocket(settings);
   const { socketRef } = websocket;
@@ -46,15 +47,21 @@ const HomePage: NextPage = () => {
   );
   const screenSize = useContext(ResponsiveContext);
   const isMobile = screenSize === "small";
-  
+
   // attach event listener for new messages
   useEffect(() => {
     if (!myPeerId || !socketRef.current) return;
-    socketRef.current.addEventListener("message", handleReceivedMessage(addReceivedMessage));
+    socketRef.current.addEventListener(
+      "message",
+      handleReceivedMessage(addReceivedMessage)
+    );
 
     return () => {
       if (!socketRef.current) return;
-      socketRef.current.removeEventListener("message", handleReceivedMessage(addReceivedMessage));
+      socketRef.current.removeEventListener(
+        "message",
+        handleReceivedMessage(addReceivedMessage)
+      );
     };
   }, [myPeerId, socketRef.current]);
 
@@ -62,7 +69,7 @@ const HomePage: NextPage = () => {
   useEffect(() => {
     loadWelcomeConversation();
     //(development == 'enabled' || process.env.NODE_ENV != 'production') && loadDevHelperConversation();
-  }, [development])
+  }, [development]);
 
   return (
     <Box fill direction="row" justify="between" pad="small">
@@ -88,6 +95,7 @@ const HomePage: NextPage = () => {
           }}
           addNewConversation={handleAddNewConversation(() => setFocus("chat"))}
           counterparties={Array.from(conversations.keys())}
+          hoprBalance={hoprBalance}
         />
       </Box>
       <Box
@@ -113,7 +121,11 @@ const HomePage: NextPage = () => {
           selection={selection}
           apiEndpoint={settings.apiEndpoint}
           messages={conversation ? Array.from(conversation.values()) : []}
-          sendMessage={handleSendMessage(addSentMessage)(myPeerId, socketRef, getReqHeaders(true))}
+          sendMessage={handleSendMessage(addSentMessage)(
+            myPeerId,
+            socketRef,
+            getReqHeaders(true)
+          )}
         />
       </Box>
     </Box>
